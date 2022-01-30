a036f4c7992e2e22950314d8b5ce2ea2ce17b546
Marco Castrellon <marcocastrellon@hotmail.com>
2022-01-30 11:57:38 -0300

    Subiendo desafio complementario clase 3

---
+++ js.js
@@ -0,0 +1,13 @@
+let edad = prompt("Ingrese su edad");
+
+while (edad < 18) {
+
+    alert("Usuario menor de edad. Acceso denegado.")
+
+    edad = prompt("Ingrese su edad")
+
+}
+
+if (edad >= 18) {
+    alert("Usuario mayor de edad. Acceso permitido.")
+}
